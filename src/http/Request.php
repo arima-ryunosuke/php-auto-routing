5437226dd1152fb8515c14dc8735769acd412f5b
ryunosuke <ryunosuke.arima@gmail.com>
2023-07-21 22:38:49 +0900

    [feature] Request にユーティリティを追加（input/any/only/except）

--- src/http/Request.php
+++ src/http/Request.php
@@ -15,12 +15,16 @@ class Request extends \Symfony\Component\HttpFoundation\Request
     /** @var InputBag alias for $this->>request */
     public $post;
 
+    /** @var InputBag alias by Request Method */
+    public $input;
+
     public function __construct(array $query = [], array $request = [], array $attributes = [], array $cookies = [], array $files = [], array $server = [], $content = null)
     {
         parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
 
         $this->get = $this->query;
         $this->post = $this->request;
+        $this->input = $this->isMethod('GET') ? $this->query : $this->request;
     }
 
     public function __get($name)
@@ -49,6 +53,61 @@ class Request extends \Symfony\Component\HttpFoundation\Request
         }
 
         return $default;
+    }
+
+    /**
+     * 現在のリクエストメソッドパラメータから最初に見つかったものを返す
+     *
+     * Symfony の型の制約を受けない（配列型でもそのまま返す）。
+     * 無かった場合は null 固定。
+     *
+     * @param string ...$keys キー名
+     * @return string|int|float|bool|null リクエストの値
+     */
+    public function any(string ...$keys)
+    {
+        $all = $this->input->all();
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $all)) {
+                return $all[$key];
+            }
+        }
+        return null;
+    }
+
+    /**
+     * 現在のリクエストメソッドパラメータから指定したもののみを返す
+     *
+     * Symfony の型の制約を受けない（配列型でもそのまま返す）。
+     * keys の指定順は維持される。
+     *
+     * @param string ...$keys キー名
+     * @return array リクエストの値
+     */
+    public function only(string ...$keys)
+    {
+        //return array_intersect_key($this->input->all(), array_keys($keys));
+        $all = $this->input->all();
+        $result = [];
+        foreach ($keys as $key) {
+            if (array_key_exists($key, $all)) {
+                $result[$key] = $all[$key];
+            }
+        }
+        return $result;
+    }
+
+    /**
+     * 現在のリクエストメソッドパラメータから指定したものを除外してを返す
+     *
+     * Symfony の型の制約を受けない（配列型でもそのまま返す）。
+     *
+     * @param string ...$keys キー名
+     * @return array リクエストの値
+     */
+    public function except(string ...$keys)
+    {
+        return array_diff_key($this->input->all(), array_flip($keys));
     }
 
     public function getUserAgent()
--- tests/Test/microute/http/RequestTest.php
+++ tests/Test/microute/http/RequestTest.php
@@ -54,6 +54,53 @@ class RequestTest extends \ryunosuke\Test\AbstractTestCase
         $this->assertEquals('defval', $request->input('undefined', 'defval'));
     }
 
+    function test_any()
+    {
+        $request = new Request(['hoge' => 'HOGE', 'fuga' => 'FUGA', 'array' => [1, 2, 3]]);
+
+        $this->assertEquals('HOGE', $request->any('unknown', 'hoge', 'fuga'));
+        $this->assertEquals('FUGA', $request->any('unknown', 'fuga', 'hoge'));
+        $this->assertEquals([1, 2, 3], $request->any('unknown', 'array', 'hoge'));
+        $this->assertEquals(null, $request->any('unknown', 'unknown1', 'unknown2'));
+    }
+
+    function test_only_except()
+    {
+        $request = new Request(
+            ['hoge' => 'get-HOGE', 'fuga' => 'get-FUGA', 'piyo' => 'get-PIYO', 'array' => [1, 2, 3]],
+            ['hoge' => 'post-HOGE', 'fuga' => 'post-FUGA', 'piyo' => 'post-PIYO', 'array' => [4, 5, 6]],
+            [], [], [],
+            ['REQUEST_METHOD' => 'GET'],
+        );
+
+        $this->assertSame([
+            'piyo'  => 'get-PIYO',
+            'array' => [1, 2, 3],
+        ], $request->only('piyo', 'array', 'unknown'));
+
+        $this->assertSame([
+            'hoge'  => 'get-HOGE',
+            'array' => [1, 2, 3],
+        ], $request->except('piyo', 'fuga', 'unknown'));
+
+        $request = new Request(
+            ['hoge' => 'get-HOGE', 'fuga' => 'get-FUGA', 'piyo' => 'get-PIYO', 'array' => [1, 2, 3]],
+            ['hoge' => 'post-HOGE', 'fuga' => 'post-FUGA', 'piyo' => 'post-PIYO', 'array' => [4, 5, 6]],
+            [], [], [],
+            ['REQUEST_METHOD' => 'POST'],
+        );
+
+        $this->assertSame([
+            'piyo'  => 'post-PIYO',
+            'array' => [4, 5, 6],
+        ], $request->only('piyo', 'array', 'unknown'));
+
+        $this->assertSame([
+            'hoge'  => 'post-HOGE',
+            'array' => [4, 5, 6],
+        ], $request->except('piyo', 'fuga', 'unknown'));
+    }
+
     function test_getUserAgent()
     {
         $request = new Request([], [], [], [], [], [
